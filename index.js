6fc9aefbb1cf2f8fd8a4c0c44c2ae4f2b865ce8e
Bing <bx.liu@outlook.com>
2024-11-19 21:16:37 -0500

    implemented code to watch a specific div

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 import nodemailer from "nodemailer"
 import "dotenv/config"
-import {logger} from "./logger.js"
+import { logger } from "./logger.js"
 
 // console.log("starting...")
 logger.info("starting...")
@@ -20,29 +20,29 @@ let transporter = nodemailer.createTransport({
 });
 
 //async..await is not allowed in global scope, must use a wrapper
-async function sendReminder(){
+async function sendReminder() {
   // send mail with defined transport object
   let info = await transporter.sendMail({
     from: process.env.EMAIL_SENDER, // sender address
     to: process.env.EMAIL_RECEIVER, // list of receivers
-    subject: "Smoked Meat Tuesday", // Subject line
-    html: "Hello, This is a friendly reminder that <h3>🔥TOMORROW🔥</h3> the smoked meat plater is <b>10.99$</b> at you know where", // html body
+    subject: "Tay Bot", // Subject line
+    html: "Hello, this is a test", // html body
   });
   logger.info("Message sent: %s", info.messageId);
 }
 
 //Check if the day is Monday, if so, run the email mailer
-function validateDate(){
+function validateDate() {
   let d = new Date(Date())
-  d.setDate(d.getDate()+1) //set the day 1 day in the future
+  d.setDate(d.getDate() + 1) //set the day 1 day in the future
   const date = d.getDate()
   const day = d.getDay()
-  if(date<=7 && day === 2){
+  if (date <= 7 && day === 2) {
     //run below if "tomorrow" is the first Tuesday of the month
-    logger.warn({tomorrow: d, jsDate: date, jsDay: day}, "Smoke Meat Day!")
+    logger.warn({ tomorrow: d, jsDate: date, jsDay: day }, "Smoke Meat Day!")
     sendReminder().catch(console.error)
-  }else{
-    logger.info({tomorrow: d, jsDate: date, jsDay: day}, "no joy!")
+  } else {
+    logger.info({ tomorrow: d, jsDate: date, jsDay: day }, "no joy!")
   }
 }
 
@@ -51,9 +51,43 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// const { Builder, Browser, By, Key, until } = require('selenium-webdriver')
+import { Builder, Browser, By, Key, until } from "selenium-webdriver"
+
+  ; (async function example() {
+
+    let innerText
+    let driver = await new Builder()
+      .forBrowser(Browser.FIREFOX)
+      .build();
+    while (true) {
+      try {
+        await driver.get('https://www.ticketmaster.ca/taylor-swift-the-eras-tour-toronto-ontario-11-21-2024/event/10005F01FC4E4BD4')
+
+        const xpath = "//div[contains(text(), 'Sorry, tickets are not currently available online.')]";
+        const divElement = await driver.wait(until.elementLocated(By.xpath(xpath)), 500);
+        console.log('Success: Div found')
+        // const divText = await divElement.getText();
+        // console.log('Found div with text:', divText);
+      }catch (error) {
+        if (error.name === 'NoSuchElementError') {
+          console.log('Failed to find the div element with the specified class name');
+        } else {
+          console.error('#An error occurred:', error.message);
+      }
+        sleep(1000)
+
+      } finally {
+        //await driver.quit()
+
+      }
+    }
+  })()
+
+
 
 //Main loop
-while(true){
-  await sleep(1000);//3000ms
-  validateDate();
-}
+// while(true){
+//   await sleep(1000);//3000ms
+//   //validateDate();
+// }
